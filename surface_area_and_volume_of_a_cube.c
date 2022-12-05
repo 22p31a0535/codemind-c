d9324b2efd719c677356cc38772837897abd1889
22p31a0535 <119732523+22p31a0535@users.noreply.github.com>
2022-12-05 21:40:24 +0530

    #include<stdio.h> #include<math.h> int main() {     int a;     scanf("%d",&a);     float s,v;     s=(6*a*a);     v=pow(a,3);     printf("Surface area = %.2f and Volume = %.2f",s,v);    // printf("Volume = %.2f",v); }

---
+++ surface_area_and_volume_of_a_cube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    float s,v;
+    s=(6*a*a);
+    v=pow(a,3);
+    printf("Surface area = %.2f and Volume = %.2f",s,v);
+   // printf("Volume = %.2f",v);
+}
