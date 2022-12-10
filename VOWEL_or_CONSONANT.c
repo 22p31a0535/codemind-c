e53ef6e6b8bb441d4c017612a8f1c6f17b0195c2
22p31a0535 <119732523+22p31a0535@users.noreply.github.com>
2022-12-10 12:51:04 +0530

    #include<stdio.h> int main() {     char k;     scanf("%c",&k);     if(k=='a'||k=='e'||k=='i'||k=='o'||k=='u')     printf("VOWEL");     else     printf("CONSONANT"); }

--- VOWEL_or_CONSONANT.c
+++ VOWEL_or_CONSONANT.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    char ch;
-    scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
-    {
-        printf("VOWEL");
-    }
-else
-{
+    char k;
+    scanf("%c",&k);
+    if(k=='a'||k=='e'||k=='i'||k=='o'||k=='u')
+    printf("VOWEL");
+    else
     printf("CONSONANT");
-}
 }
