4534efbfc08e656ce54655729883a37ac7ca8a97
22p31a0535 <119732523+22p31a0535@users.noreply.github.com>
2022-12-30 14:44:30 +0530

    #include<stdio.h> int main() {     int m,n,i,r=0;     scanf("%d%d",&m,&n);     for(i=m;i<=n;i++)     {         if(i%3==0)         {             r++;         }     }     printf("%d",r); }

--- count_Multiples_of_3_between_M_,N.c
+++ count_Multiples_of_3_between_M_,N.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-  int m,n,i,c=0;
-  scanf("%d%d",&m,&n);
-  for(i=m;i<=n;i++)
-  {
-      if(i%3==0)
-      {
-          c++;
-      }
-  }
-  printf("%d",c);
+    int m,n,i,r=0;
+    scanf("%d%d",&m,&n);
+    for(i=m;i<=n;i++)
+    {
+        if(i%3==0)
+        {
+            r++;
+        }
+    }
+    printf("%d",r);
 }
